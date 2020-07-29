8cb4bc9c3a415a59ce25341e9c65c2fc94d4f2d1
yonatan kreiner <yonatankreiner@gmail.com>
2020-07-29 19:04:23 +0300

    added singleton

---
+++ creational/singleton/singleton.ts
@@ -0,0 +1,15 @@
+class Singleton {
+    private static instance: Singleton;
+
+    private constructor() { }
+
+    public static getInstance(): Singleton {
+        if (!Singleton.instance) {
+            Singleton.instance = new Singleton();
+        }
+
+        return Singleton.instance;
+    }
+}
+
+export default Singleton;
